5c88e045543b82bbda58317519dc571ab695b9b0
Moon Davé <moon@softmoon.world>
2026-02-16 09:02:11 -0500

    stub midi example code

--- crates/processing_midi/src/lib.rs
+++ crates/processing_midi/src/lib.rs
@@ -1,9 +1,25 @@
 use bevy::prelude::*;
+use bevy_midi::prelude::*;
 
 pub struct MidiPlugin;
 
 impl Plugin for MidiPlugin {
     fn build(&self, app: &mut App) {
-        dbg!(app);
+        app.insert_resource(MidiOutputSettings {
+            port_name: "output",
+        })
+        .add_plugins(MidiOutputPlugin);
     }
 }
+
+enum MidiCommand {}
+
+pub fn connect(port: usize) {
+    // we need to work with the ECS
+    // do we pass a MidiCommand to Bevy?
+}
+
+pub fn disconnect() {}
+pub fn refresh_ports() {}
+
+pub fn play_notes() {}
