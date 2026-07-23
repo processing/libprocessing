ac2628858cd85dc3c10ef2121dddcb6b78046ef9
charlotte 🌸 <charlotte.c.mcelwain@gmail.com>
2026-07-23 14:29:23 -0700

    Bring wasm up to date (#210)

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,6 +4,7 @@ pub use bevy::prelude::default;
 pub use bevy::render::render_resource::TextureFormat;
 pub use processing_core::{config::*, error};
 pub use processing_input::*;
+#[cfg(not(target_arch = "wasm32"))]
 pub use processing_midi::{
     midi_connect, midi_disconnect, midi_list_ports, midi_note_off, midi_note_on, midi_play_notes,
     midi_refresh_ports,
